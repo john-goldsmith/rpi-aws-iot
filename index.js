c1c234936d8567d5f98c9c15968fc3c9b5834147
John Goldsmith <john.goldsmith@verys.com>
2016-02-10 21:46:09 -0800

    Fix typo; add response status code

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ var express = require('express'),
 app.set('port', (process.env.PORT || 5000));
 
 app.post('/', function (req, res) {
+  res.status(200);
   res.json({
-    foo: 'bar'
+    response_type: 'in_channel',
+    text: 'It\'s 80 degrees right now.',
+    attachments: [
+      {
+        title: 'Lounge status',
+        title_link: 'https://s3-us-west-2.amazonaws.com/rpi-aws-iot/camera.jpg',
+        image_url: 'https://s3-us-west-2.amazonaws.com/rpi-aws-iot/camera.jpg'
+      }
+    ]
   });
 });
 
